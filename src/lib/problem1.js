3216bcb57be3ee0ecc743c38be7a5b2a15340d33
AlyxMoon <allisterkmoon@gmail.com>
2021-09-03 08:31:46 -0700

    solve problem 1 iteratively, split test between iterative/recursive

--- src/lib/problem1.js
+++ src/lib/problem1.js
@@ -1,6 +1,22 @@
 
-const turnStringsToUrls = (string) => {
+const turnStringsToUrlsIterative = (string) => {
+  let result = ''
+
+  for (const char of string) {
+    result += char === ' ' ? '%20' : char
+  }
+
+  return result
+}
+
+const turnStringsToUrlsRecursion = (string) => {
   return string
 }
 
-export default turnStringsToUrls
+const solution = (input, { recursion = false } = {}) => {
+  return recursion
+    ? turnStringsToUrlsRecursion(input)
+    : turnStringsToUrlsIterative(input)
+}
+
+export default solution
--- tests/unit/problem1.test.js
+++ tests/unit/problem1.test.js
@@ -1,23 +1,52 @@
 import solution from '@/lib/problem1'
 
 describe('problem 1', () => {
-  it('returns an empty string if input empty', () => {
-    expect(solution('')).toEqual('')
+  describe('iterative', () => {
+    it('returns an empty string if input empty', () => {
+      expect(solution('')).toEqual('')
+    })
+
+    it('returns a string exactly the same if no spaces', () => {
+      expect(solution('hello-there_friend1')).toEqual('hello-there_friend1')
+    })
+
+    it('correctly replaces spaces with %20', () => {
+      expect(solution('hello there world')).toEqual('hello%20there%20world')
+    })
+
+    it('correctly replaces multiple spaces with %20', () => {
+      expect(solution('  hello   there  ')).toEqual('%20%20hello%20%20%20there%20%20')
+    })
+
+    it('does not impact existing %20', () => {
+      expect(solution('hello%20there ')).toEqual('hello%20there%20')
+    })
   })
 
-  it('returns a string exactly the same if no spaces', () => {
-    expect(solution('hello-there_friend1')).toEqual('hello-there_friend1')
-  })
+  describe('recursive', () => {
+    it('returns an empty string if input empty', () => {
+      const options = ['', { recursion: true }]
+      expect(solution(...options)).toEqual('')
+    })
 
-  it('correctly replaces spaces with %20', () => {
-    expect(solution('hello there world')).toEqual('hello%20there%20world')
-  })
+    it('returns a string exactly the same if no spaces', () => {
+      const options = ['hello-there_friend1', { recursion: true }]
+      expect(solution(...options)).toEqual('hello-there_friend1')
+    })
 
-  it('correctly replaces multiple spaces with %20', () => {
-    expect(solution('  hello   there  ')).toEqual('%20%20hello%20%20%20there%20%20')
-  })
+    it('correctly replaces spaces with %20', () => {
+      const options = ['hello there world', { recursion: true }]
+      expect(solution(...options)).toEqual('hello%20there%20world')
+    })
 
-  it('does not impact existing %20', () => {
-    expect(solution('hello%20there ')).toEqual('hello%20there%20')
+    it('correctly replaces multiple spaces with %20', () => {
+      const options = ['  hello   there  ', { recursion: true }]
+      expect(solution(...options)).toEqual('%20%20hello%20%20%20there%20%20')
+    })
+
+    it('does not impact existing %20', () => {
+      const options = ['hello%20there ', { recursion: true }]
+      expect(solution(...options)).toEqual('hello%20there%20')
+    })
   })
 })
