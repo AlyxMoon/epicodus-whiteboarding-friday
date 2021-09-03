65f54ea487d8891af78e69cfe00da17d036cd40c
AlyxMoon <allisterkmoon@gmail.com>
2021-09-03 12:10:00 -0700

    implement quicksort

--- src/lib/problem5.js
+++ src/lib/problem5.js
@@ -1,6 +1,44 @@
 
 const sortByQuick = (input = []) => {
-  return input
+  if (input.length < 2) return input
+
+  const switchElements = (arr, i1, i2) => {
+    const temp = arr[i1]
+    arr[i1] = arr[i2]
+    arr[i2] = temp
+  }
+
+  const doQuickSortPartition = (arr, start, end) => {
+    const pivotIndex = start
+    const pivotElement = arr[start]
+
+    while (start < end) {
+      while (start < arr.length && arr[start] <= pivotElement) {
+        start++
+      }
+
+      while (arr[end] > pivotElement) end--
+
+      if (start < end) switchElements(arr, start, end)
+    }
+
+    switchElements(arr, pivotIndex, end)
+    return end
+  }
+
+  const doQuickSort = (arr, start, end) => {
+    if (start < 0 && end < 0) return arr
+    if (start >= end) return arr
+
+    const pivotIndex = doQuickSortPartition(arr, start, end)
+
+    doQuickSort(arr, start, pivotIndex - 1)
+    doQuickSort(arr, pivotIndex + 1, end)
+
+    return arr
+  }
+
+  return doQuickSort(input.slice(), 0, input.length - 1)
 }
 
 const sortByMerge = (input = []) => {
@@ -24,8 +62,12 @@ const sortBySelection = (input = []) => {
 }
 
 const solution = (input, { type = '' } = {}) => {
-  if (typeof input === 'number') {
-    throw new Error('Only numbers are accepted!')
+  if (!Array.isArray(input)) {
+    throw new Error('Only arrays are accepted!')
+  }
+
+  if (input.some(input => typeof input !== 'number')) {
+    throw new Error('There should only be numbers in the array!')
   }
 
   switch (type) {
