3a6621a9a658585844fd06f4d591eac9501f3a74
AlyxMoon <allisterkmoon@gmail.com>
2021-09-03 10:27:09 -0700

    write tests for problem 3

--- tests/unit/problem3.test.js
+++ tests/unit/problem3.test.js
@@ -1,5 +1,47 @@
+import solution from '@/lib/problem3'
+
 describe('problem 3', () => {
-  it('test', () => {
-    expect(true).toEqual(true)
+  const tests = [
+    {
+      description: 'should work with an empty string',
+      input: '',
+      output: '',
+    },
+    {
+      description: 'should return the same string if no repeated characters',
+      input: 'abc',
+      output: 'abc',
+    },
+    {
+      description: 'should correctly compress repeated characters',
+      input: 'aaaabc',
+      output: '4abc',
+    },
+    {
+      description: 'should correctly compress different repeated characters',
+      input: 'aabbzzzzzcc',
+      output: '2a2b5z2c',
+    },
+    {
+      description: 'should correctly compress the same repeated character when separated',
+      input: 'aabbaaa',
+      output: '2a2b3a',
+    },
+  ]
+
+  describe('iterative', () => {
+    tests.forEach(({ description, input, output }) => {
+      it(description, () => {
+        expect(solution(input)).toEqual(output)
+      })
+    })
+  })
+
+  describe('recursive', () => {
+    tests.forEach(({ description, input, output }) => {
+      it(description, () => {
+        expect(solution(input, { recursion: true })).toEqual(output)
+      })
+    })
   })
 })
