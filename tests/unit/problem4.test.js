1701577e308d022b6de25bb8c298f2e5e3caa5e6
AlyxMoon <allisterkmoon@gmail.com>
2021-09-03 10:58:10 -0700

    add tests for problem 4

--- tests/unit/problem4.test.js
+++ tests/unit/problem4.test.js
@@ -1,5 +1,19 @@
+import solution from '@/lib/problem4'
+
 describe('problem 4', () => {
-  it('test', () => {
-    expect(true).toEqual(true)
+  it('returns true for an empty string', () => {
+    expect(solution('')).toEqual(true)
+  })
+
+  it('returns true for a string with all unique values', () => {
+    expect(solution('abcdefghijklmnopqrstuvwxyz')).toEqual(true)
+  })
+
+  it('returns false for a string with a consecutive duplicate value', () => {
+    expect(solution('abcdefa')).toEqual(false)
+  })
+
+  it('returns false for a string with a non-consecutive duplicate value', () => {
+    expect(solution('abcdeaf')).toEqual(false)
   })
 })
